c29ae72446a1cf333e1eb7e618e47ee551647aea
Yansaro Rodriguez <yansarorodriguez@Yansaros-Mini.attlocal.net>
2022-11-05 15:43:53 -0400

    feat(setup): initial setup

---
+++ lib/src/case_matcher.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// {@template case_matcher}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class CaseMatcher {
+  /// {@macro case_matcher}
+  const CaseMatcher();
+}
