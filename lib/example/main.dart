463b6f2ad0dfe4831a419ca2425904cf472f16de
Yansaro Rodriguez <yansarorodriguez@Yansaros-Mac-mini.local>
2022-12-03 18:25:10 -0500

    refactor(example): change example for one using operators

--- lib/example/main.dart
+++ lib/example/main.dart
@@ -1,39 +1,43 @@
-import 'dart:developer';
-import 'dart:math' show Random;
-
 import 'package:case_matcher/case_matcher.dart';
-import 'package:case_matcher/src/extensions/extensions.dart';
 import 'package:matcher/matcher.dart';
 
-class IntMatcher extends CaseMatcher<int, String> {
-  IntMatcher()
-      : super(
-          onDefault: (element) => 'Could not match any case : $element',
-        ) {
+void main() {
+  final intToStringMatcher1 = CaseMatcher<int, String>(
+    onDefault: (element) => 'Could not match any case : $element',
+  )
     // Just simple specifications.
-    // An extension is provided for this unary matchers so you don't have to
-    // write onCase(matcher).
-    onEquals(10, (value) => 'Is exactly 10');
-    onNotEquals(5, (value) => 'Is not exactly 5');
-    onGreaterThan(5, (value) => '$value is greater than 5');
+    ..onEquals(10, (value) => 'Is exactly 10')
+    ..onEquals(5, (value) => 'Is exactly 5')
+    ..onGreaterThan(5, (value) => '$value is greater than 5');
 
-    // More complex specifications
-    onCase(
+  final resultFor5 = intToStringMatcher1 << 5; // Is exactly 5
+  print('5: $resultFor5');
+  final resultFor10 = intToStringMatcher1 << 10; // Is exactly 10
+  print('10: $resultFor10');
+  final resultFor6 = intToStringMatcher1 << 6; // 6 is greater than 5
+  print('6: $resultFor6');
+  final intToStringMatcher2 = CaseMatcher<int, String>(
+    onDefault: (element) => 'Could not match any case : $element',
+  ) // More complex specifications
+    ..onCase(
       greaterThan(5) & lessThan(10),
       (value) => '$value is greater than 5 and less than 10',
+    )
+    ..onCase(
+      greaterThan(10) | lessThan(5),
+      (value) => '$value is greater than 10 or less than 5',
     );
-    onCase(
-      greaterThan(5) | lessThan(10),
-      (value) => '$value is greater than 5 or less than 10',
-    );
-  }
-}
 
-void main() {
-  final objects = List.generate(100, (index) => index);
+  final resultFor7 =
+      intToStringMatcher2 << 7; // 7 is greater than 5 and less than 10
+  print('7: $resultFor7');
+  final resultFor11 =
+      intToStringMatcher2 << 11; // 11 is greater than 10 or less than 5
+  print('11: $resultFor11');
+  final resultFor4 =
+      intToStringMatcher2 << 4; // 4 is greater than 10 or less than 5
+  print('4: $resultFor4');
 
-  final objectToTest = objects[Random().nextInt(objects.length)];
-  final toStringMatcher = IntMatcher();
-  final result = toStringMatcher.match(objectToTest);
-  log('El type de ${objectToTest} es: ${result}');
+  final matchersCombined = intToStringMatcher1 +
+      intToStringMatcher2; // Contains all the cases from both matchers
 }
