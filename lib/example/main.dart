b4092a0d28608adb9e48f338205df264537a8b19
Yansaro Rodriguez <yansarorodriguez@Yansaros-Mac-mini.local>
2022-12-03 20:43:05 -0500

    refactor(example_main): provide a much cooler example

--- lib/example/main.dart
+++ lib/example/main.dart
@@ -2,42 +2,21 @@ import 'package:case_matcher/case_matcher.dart';
 import 'package:matcher/matcher.dart';
 
 void main() {
-  final intToStringMatcher1 = CaseMatcher<int, String>(
-    onDefault: (element) => 'Could not match any case : $element',
+  const number = 3;
+
+  final matcher = CaseMatcher<int, String>(
+    onDefault: (_) => "Ain't special",
   )
     // Just simple specifications.
-    ..onEquals(10, (value) => 'Is exactly 10')
-    ..onEquals(5, (value) => 'Is exactly 5')
-    ..onGreaterThan(5, (value) => '$value is greater than 5');
-
-  final resultFor5 = intToStringMatcher1 << 5; // Is exactly 5
-  print('5: $resultFor5');
-  final resultFor10 = intToStringMatcher1 << 10; // Is exactly 10
-  print('10: $resultFor10');
-  final resultFor6 = intToStringMatcher1 << 6; // 6 is greater than 5
-  print('6: $resultFor6');
-  final intToStringMatcher2 = CaseMatcher<int, String>(
-    onDefault: (element) => 'Could not match any case : $element',
-  ) // More complex specifications
-    ..onCase(
-      greaterThan(5) & lessThan(10),
-      (value) => '$value is greater than 5 and less than 10',
+    ..onEquals(1, (value) => 'The very first unit')
+    ..onNotEquals(
+      [2, 3, 5, 7, 11, 13],
+      (value) => 'This is not a prime less than 11',
     )
     ..onCase(
-      greaterThan(10) | lessThan(5),
-      (value) => '$value is greater than 10 or less than 5',
+      greaterThanOrEqualTo(11) & lessThanOrEqualTo(19),
+      (value) => 'A teen',
     );
-
-  final resultFor7 =
-      intToStringMatcher2 << 7; // 7 is greater than 5 and less than 10
-  print('7: $resultFor7');
-  final resultFor11 =
-      intToStringMatcher2 << 11; // 11 is greater than 10 or less than 5
-  print('11: $resultFor11');
-  final resultFor4 =
-      intToStringMatcher2 << 4; // 4 is greater than 10 or less than 5
-  print('4: $resultFor4');
-
-  final matchersCombined = intToStringMatcher1 +
-      intToStringMatcher2; // Contains all the cases from both matchers
+  // ignore: unused_local_variable
+  final result = matcher << number;
 }
